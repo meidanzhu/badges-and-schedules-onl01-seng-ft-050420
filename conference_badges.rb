36045d4e3910e888cc0aafd9d3113f2651940fbf
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-14 18:17:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,6 +11,7 @@ def batch_badge_creator(names)
   return badge_messages
 end
 
+<<<<<<< HEAD
 def assign_rooms(speakers)
     message = []
     speakers.each_with_index{ |speakers, index|
@@ -20,6 +21,19 @@ def assign_rooms(speakers)
 
   def printer(name)
     batch_badge_creator(name).each do |badges|
+=======
+def assign_rooms(speaker)
+    rooms = []
+    speakers.each_with_index(1) do |speaker, room_assignment|
+    rooms << room_assignment = "Hello, #{speaker}! You'll be assigned to #{room_assignment}!"}
+    end
+  return rooms
+end
+
+
+  def printer(name)
+    batch_badge_creator(name).each do |badge_messages|
+>>>>>>> 58db4ddec33e51b8df0c7cfa34762a591acd8d9b
       puts badges
     end
     assign_rooms(name).each do |room_assignment|
